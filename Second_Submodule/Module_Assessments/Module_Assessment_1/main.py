0b9fd0ecabd2c5aa863422988e6632aa6a05e2a9
Abdelrahman <a.mahmoud.a01@gmail.com>
2024-12-26 03:34:21 +0200

    Second module second project

--- Second_Submodule/Module_Assessments/Module_Assessment_1/main.py
+++ Second_Submodule/Module_Assessments/Module_Assessment_1/main.py
@@ -2,4 +2,4 @@ start_year = int(input("Please enter the start_year: "))
 end_year = int(input("Please enter the end-year: "))
 age = end_year - start_year
 
-print(f"The age is {age}")
+print(f"The age is {age}")A
---
+++ Second_Submodule/Module_Assessments/Module_Assessment_2/main.py
@@ -0,0 +1,6 @@
+first_number = int(input("Please enter the first_number: "))
+second_number = int(input("Please enter the second_number: "))
+print(f'''The sum = {first_number+second_number} 
+The subtraction = {first_number-second_number}
+The multiplication = {first_number*second_number} 
+The division = {first_number/second_number} ''')
