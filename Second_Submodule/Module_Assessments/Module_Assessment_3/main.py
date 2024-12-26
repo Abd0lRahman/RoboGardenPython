fbea8938aeb29c8ffcb741dc7678c0b6ab7f3ba3
Abdelrahman <a.mahmoud.a01@gmail.com>
2024-12-26 03:42:55 +0200

    Second module Third project

---
+++ Second_Submodule/Module_Assessments/Module_Assessment_3/main.py
@@ -0,0 +1,5 @@
+radius = int(input("Please enter the radius: "))
+circumference = 2 * radius * 3.142
+area = 3.142 * radius * radius
+print(f'''The circumference = {circumference} 
+The area = {area}''')
